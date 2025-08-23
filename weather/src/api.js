26b3ab85bd48e4d2bb82a07527539e3d418868cc
mojitaher <mojtabataherkhani80@gmail.com>
2025-08-23 18:37:23 +0330

    env.local is set

--- weather/src/api.js
+++ weather/src/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-const API_KEY = "b2fc5ba075dd443ca7b125246252008";
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+const baseUrl = process.env.NEXT_PUBLIC_API_BASE;
 
 export async function getApiForcast(city) {
-  const res = await axios.get(
-    `http://api.weatherapi.com/v1/forecast.json?q=${city}&key=${API_KEY}&days=3`
-  );
+  const res = await axios.get(`${baseUrl}?q=${city}&key=${API_KEY}&days=3`);
   return res.data;
 }
