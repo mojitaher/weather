4f7fd560344aa645ae0754c9472e43151223401d
mojitaher <mojtabataherkhani80@gmail.com>
2025-08-23 18:46:48 +0330

    fixed

--- weather/src/api.js
+++ weather/src/api.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
-const baseUrl = process.env.NEXT_PUBLIC_API_BASE;
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE;
 
 export async function getApiForcast(city) {
-  const res = await axios.get(`${baseUrl}?q=${city}&key=${API_KEY}&days=3`);
+  const res = await axios.get(
+    `${baseUrl}/forecast.json?q=${city}&key=${API_KEY}&days=3`
+  );
   return res.data;
 }
